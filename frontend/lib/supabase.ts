4245c1600bc5c7005484eebf8f3c2f85067e0926
vicrichy87 <vicrichy87@gmail.com>
2025-09-12 16:13:52 +0100

    Update Supabase credentials

--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  'YOUR_SUPABASE_URL',
-  'YOUR_SUPABASE_ANON_KEY'
-);
+const supabaseUrl = "https://home-teachers-ghana.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhidWNiaHh3YXBxem9tdWx2Zm5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc3MDA3NzEsImV4cCI6MjA3MzI3Njc3MX0.k9ADjrwU0BsTUGT4UtpEuxgPwuP_T3CeSzaPknndRcY";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
