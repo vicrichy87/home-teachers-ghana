2a6af17f4a402d2f28b938d896cd86f9f4bc9584
vicrichy87 <62028881+vicrichy87@users.noreply.github.com>
2025-09-18 17:39:12 +0100

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -2,19 +2,28 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import SplashScreen from "./screens/SplashScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
-import HomeScreen from "./screens/HomeScreen"; // create a simple placeholder
+import StudentScreen from "./screens/StudentScreen";
+import TeacherScreen from "./screens/TeacherScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{
+          headerShown: false, // hide default headers since we use Banner
+        }}
+      >
+        <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Student" component={StudentScreen} />
+        <Stack.Screen name="Teacher" component={TeacherScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
